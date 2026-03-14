238639afd54b8ccef243c2c4d53d677fdfb4ed23
dagyepong <dennisagyepong2@gmail.com>
2026-03-13 22:01:55 -0500

    update

--- arch_hardened.sh
+++ arch_hardened.sh
@@ -451,5 +451,5 @@ arch-chroot /mnt systemctl --global enable gammastep
 
 # Run userspace configuration
 HOME="/home/$user" arch-chroot -u "$user" /mnt /bin/bash -c 'cd && \
-                                                             git clone https://github.com/ShellCode33/.dotfiles && \
-                                                             .dotfiles/install.sh'
+                                                             git clone https://github.com/dagyepong/workspace && \
+                                                             .arch_hardened.sh'
